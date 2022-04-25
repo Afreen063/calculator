84b75fba6bdb3b3cf208a320b5cd2d6e556ccc48
Afreen063 <afreen0501@gmail.com>
2022-04-26 00:32:23 +0530

    done basic

--- cal.js
+++ cal.js
@@ -1,12 +1,49 @@
-let input =document.getElementsByClassName("input");
-let op=document.getElementsByClassName("op");
-let num=document.getElementsById("num");
-let number=[];
-let count=0;
-num.eventlistener('click',function(){
-    number[count++]=num.innerText();
-})
-op.eventlistener('click',function()
+
+let op=0,n=0,num1=0,num2=0;
+    $('.btn').on('click', function(e) {   
+             //console.log( e.target.innerHTML);
+             n=parseInt(e.target.innerHTML);
+             display(n);
+                });
+function display(num)
 {
-   input=number[1]    
+    if(isNaN(op))
+    {
+        num2=num2*10+num;
+    $('.item-2 span').text(num1+op+num2);
+    operation();
+    }
+    else
+    {
+    num1=num1*10+num;
+    $('.item-2 span').text(num1);
+    }
+    console.log();
+}
+$('.operator').on('click', function(p){
+    op=p.target.innerHTML;
+    $('.item-2 span').text(num1+op);
 })
+function operation()
+{
+    $('.equal').on('click',function()
+    {
+    switch(op)
+    {
+        case '+':
+            $('.item-2 span').text(num1+num2);
+            break;
+        case '-':
+            $('.item-2 span').text(num1-num2);
+            break;
+        case '*':
+                $('.item-2 span').text(num1*num2);
+                break; 
+      case '/':
+            $('.item-2 span').text(num1/num2);
+            break;   
+    }
+    } );
+}
+
+
